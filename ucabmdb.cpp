628d708087f6c8b7cee353122235ba382bd7822a
Freddy Fernandez <146951144+fafernandez24@users.noreply.github.com>
2024-10-19 09:16:20 -0400

    Update1 ucabmdb.cpp
    Realice los siguientes cambios:
    
         (1) Para las estructuras y funciones, realice una librería llamada "ucabmdb_library.h" con la finalidad de tener en el programa principal solamente el algoritmo y no una gran cantidad de líneas entre funciones y estructuras.
    
         (2) Agregue a su vez la librería "fstream" para la lectura, escritura y el manejo de archivos.
    
         (3) Comencé creando un menu principal, y varios sub-menu para la rama de las peliculas, dandole al usuario de agregar peliculas.

--- ucabmdb.cpp
+++ ucabmdb.cpp
@@ -1,114 +1,88 @@
 // UCABMDB
-
 #include <iostream>
+#include <fstream>
+#include "ucabmdb_library.h"
+
 using namespace std;
-
-// STRUCTS
-
-/*El siguiente struct sirve para almacenar el conjunto de elementos de las peliculas
-  como el nombre, la fecha de estreno, el genero, la duracion y el pais.*/
-struct Movie{
-    string movie_name;
-    int movie_premiere;
-    string movie_type;
-    int movie_time;
-    string movie_country;
-
-    Movie *next_movie;
-    Movie *prev_movie;
-};
-
-/*El siguiente struct sirve para almacenar el conjunto de elemntos de las series
-  como el nombre, la fecha de estreno, el genero, la duracion, las temporadas y el pais.*/
-
-struct Series{
-    string series_name;
-    int series_premiere;
-    string series_type;
-    int series_time;
-    Season *seasons;
-    string series_country;
-
-    Series *next_series;
-    Series *prev_series;
-};
-
-/*El siguiente struct sirve para almacenar el conjunto de elementos de las temporadas dentro de las series
-  como el nombre, la fecha de estreno, los capitulos, la duracion y el pais.*/
-
-struct Season{
-    string season_name;
-    int season_premiere;
-    Chapter *chapters;
-
-    Season *next_season;
-    Season *prev_season;
-};
-
-/*El siguiente struct sirve para almacenar el conjunto de elementos de los capitulos dentro de las temporadas
-  de las series como el nombre, la fecha de estreno, los capitulos, la duracion y el pais.*/
-
-struct Chapter{
-    string chapter_name;
-    int chapter_premiere;
-    int chapter_time;
-    int chapter_num;
-
-    Chapter *next_chapter;
-    Chapter *prev_chapter;
-};
-
-/*El siguiente struct sirve para almacenar el conjunto de elementos de los usuarios
-  como el correo, la edad, el pais, la calificaciones y las reseñas.*/
-
-struct Users{
-    string email;
-    int years_old;
-    string country;
-    Qualification *qualification;
-    Review *review;
-
-    Users *next_user;
-    Users *prev_user;
-};
-
-/*El siguiente struct sirve para almacenar el conjunto de elementos de las reseñas de los usuarios
-  como la descripcion y la calificacion.*/
-
-struct Qualification{
-    string description;
-    float quali;
-
-    Qualification *next_quali;
-    Qualification *prev_quali;
-};
-
-/*El siguiente struct sirve para almacenar el conjunto de elementos de las reseñas de los usuarios
-  como la descripcion y la reseña.*/
-
-struct Review{
-    string description;
-    char review[250];
-
-    Review *next_review;
-    Review *prev_review;
-};
 
 int main(){
 
     string menu;
 
-    cout << "========================================" << endl;
-    cout << "                    MENU                " << endl;
-    cout << "========================================" << endl;
-    cout << " (1) Mostrar peliculas" << endl;
-    cout << " (2) Mostrar series" << endl;
-    cout << " (3) Agregar" << endl;
-    cout << " (4) Eliminar" << endl;
-    cout << "========================================" << endl;
-    cout << "OPCION ";
-    cin >> menu;
+  int main_option = -1;
+
+  while (main_option != 0){
+
+    main_option = mainMenu();
+
+    switch(main_option){
+      case 1: // DESPLEGAR MENU DE PELICULAS
+      {
+        Movie *movie_list = NULL;
+        int movie_option = -1;
+
+        while (movie_option != 0){
+          int movie_option = movieMenu();
+          switch (movie_option){
+            case 1: // VER PELICULAS
+            {
+              cout << "PELICULAS\n";
+              cout << "COMING SOON\n";
+              break;
+            }
+            case 2: // AGREGAR PELICULA
+            {
+              string movie_name, movie_type;
+              int movie_premiere, movie_time;
+
+                cout << "========================================\n";
+                cout << "                ADD MOVIE               \n";
+                cout << "========================================\n";
+                cout << "\tNombre: ";
+                cin >> movie_name;
+                cout << "\tEstreno: ";
+                cin >> movie_premiere;
+                cout << "\tGenero: ";
+                cin >> movie_type;
+                cout << "\tDuracion: ";
+                cin >> movie_time;
+                cout << "========================================\n";
+
+              addMovie(&movie_list, movie_name, movie_premiere, movie_type, movie_time);
+              break;
+            }
+            case 3: // ELIMINAR PELICULA
+            {
+
+              break;
+            }
+            default:
+            {
+              break;
+            }
+          }
+        }
+        break;
+      }
+      case 2:
+      {
+        cout << "SERIES MENU\n";
+        cout << "COMING SOON\n";
+        break;
+      }
+      case 3:
+      {
+        cout << "USUARIOS MENU\n";
+        cout << "COMING SOON\n";
+        break;
+      }
+      default:
+      {
+        cout << "MUCHAS GRACIAS POR VISITARNOS!\n";
+      }
+    }
+  }
+  return 0;
 
     return 0;
 }
-
