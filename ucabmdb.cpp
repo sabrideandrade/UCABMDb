d81b42ca2852f246b00c37e6db911efc4f0023fa
Freddy Fernandez <146951144+fafernandez24@users.noreply.github.com>
2024-10-27 23:20:51 -0400

    Usuarios ahora están disponibles👀
    Ya se puede agregar, eliminar y mostrar a los usuarios. Destacar que ahora cuando la lista de peliculas este vacía y se quiere eliminar alguna, saltara un mensaje de error después de ingresar el nombre de la pelicula a borrar

--- ucabmdb.cpp
+++ ucabmdb.cpp
@@ -1,4 +1,5 @@
 // UCABMDB
+
 #include <iostream>
 #include <fstream>
 #include "ucabmdb_library.h"
@@ -7,12 +8,8 @@ using namespace std;
 
 int main(){
 
-    string menu;
-
   int main_option = -1;
-
   while (main_option != 0){
-
     main_option = mainMenu();
 
     switch(main_option){
@@ -26,8 +23,7 @@ int main(){
           switch (movie_option){
             case 1: // VER PELICULAS
             {
-              cout << "PELICULAS\n";
-              cout << "COMING SOON\n";
+              printMovie(movie_list);
               break;
             }
             case 2: // AGREGAR PELICULA
@@ -35,33 +31,45 @@ int main(){
               string movie_name, movie_type;
               int movie_premiere, movie_time;
 
-                cout << "========================================\n";
-                cout << "                ADD MOVIE               \n";
-                cout << "========================================\n";
-                cout << "\tNombre: ";
-                cin >> movie_name;
-                cout << "\tEstreno: ";
-                cin >> movie_premiere;
-                cout << "\tGenero: ";
-                cin >> movie_type;
-                cout << "\tDuracion: ";
-                cin >> movie_time;
-                cout << "========================================\n";
+              cout << "========================================\n";
+              cout << "                ADD MOVIE               \n";
+              cout << "========================================\n";
+              cout << "\tNombre: ";
+              cin >> movie_name;
+              cout << "\tEstreno: ";
+              cin >> movie_premiere;
+              cout << "\tGenero: ";
+              cin >> movie_type;
+              cout << "\tDuracion: ";
+              cin >> movie_time;
+              cout << "========================================\n";
 
               addMovie(&movie_list, movie_name, movie_premiere, movie_type, movie_time);
               break;
             }
             case 3: // ELIMINAR PELICULA
             {
+              string movie_name;
+              cout << "========================================\n";
+              cout << "              DELETE MOVIE              \n";
+              cout << "========================================\n";
+              cout << "\tNombre: ";
+              cin >> movie_name;
+              cout << "========================================\n";
 
+              deleteMovie(&movie_list, movie_name);
               break;
             }
             default:
             {
+              if (movie_option == 0)
+                cout << "Regresando al menu principal...\n";
+              else cout << "VUELVE A INTENTAR\n";  
               break;
             }
           }
         }
+        main_option = -1;
         break;
       }
       case 2:
@@ -72,17 +80,70 @@ int main(){
       }
       case 3:
       {
-        cout << "USUARIOS MENU\n";
-        cout << "COMING SOON\n";
+        Users *users_list = NULL;
+        int users_option = -1;
+
+        while (users_option != 0){
+          int users_option = usersMenu();
+          switch (users_option){
+            case 1: // VER PELICULAS
+            {
+              printUsers(users_list);
+              break;
+            }
+            case 2: // AGREGAR PELICULA
+            {
+              string email, country;
+              int years_old;
+
+              cout << "========================================\n";
+              cout << "                ADD USER                \n";
+              cout << "========================================\n";
+              cout << "\tEmail: ";
+              cin >> email;
+              cout << "\tEdad: ";
+              cin >> years_old;
+              cout << "\tPais: ";
+              cin >> country;
+              cout << "========================================\n";
+
+              addUsers(&users_list, email, years_old, country);
+              break;
+            }
+            case 3: // ELIMINAR PELICULA
+            {
+              string email;
+              cout << "========================================\n";
+              cout << "              DELETE USER               \n";
+              cout << "========================================\n";
+              cout << "\tEmail: ";
+              cin >> email;
+              cout << "========================================\n";
+
+              deleteUsers(&users_list, email);
+              break;
+            }
+            default:
+            {
+              if (users_option == 0) cout << "Regresando al menu principal...\n";
+              else cout << "VUELVE A INTENTAR\n";  
+              break;
+            }
+          }
+        }
+        main_option = -1;
+        break;
         break;
       }
       default:
       {
-        cout << "MUCHAS GRACIAS POR VISITARNOS!\n";
+        if (main_option == 0) cout << "GRACIAS POR VISITARNOS!\n";
+        else cout << "VUELVE A INTENTAR!\n";
+        break;
       }
     }
   }
-  return 0;
 
-    return 0;
+  system("pause");
+  return 0;
 }
